3ebc0b3a5ad952f9b5dec043bd97c50f946b60dc
tfshorifulislam <tfshorifulislam@gmail.com>
2026-03-06 14:02:25 +0600

    modal product details function add

--- script/index.js
+++ script/index.js
@@ -38,19 +38,19 @@ const selectCategory = (categoryId, newBtn) => {
     const allSectionButton = document.querySelectorAll('.all-tress-btn')
 
     allSectionButton.forEach(btn => {
-        
+
         btn.classList.remove('bg-[#15803D]', 'text-white')
     });
-        newBtn.classList.add('bg-[#15803D]', 'text-white')
+    newBtn.classList.add('bg-[#15803D]', 'text-white')
 
-        const url = `https://openapi.programming-hero.com/api/category/${categoryId}`
-        fetch(url)
-            .then(res => res.json())
-            .then(data => plantCategory(data.plants))
-       
+    const url = `https://openapi.programming-hero.com/api/category/${categoryId}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => plantCategory(data.plants))
+
 }
 
-const plantCategory = (plant)=>{
+const plantCategory = (plant) => {
     allPlantCards(plant)
     hiddenSpinner()
 }
@@ -75,10 +75,10 @@ const allPlantCards = (cards) => {
         
         <div class="bg-white p-4 rounded-2xl space-y-3">
                         <div>
-                            <img class="w-full h-[187px] rounded-lg" src="${plantCards.image}" alt="">
+                            <img onclick="displayModal(${plantCards.id})" class="w-full h-[187px] rounded-lg cursor-pointer" src="${plantCards.image}" alt="">
                         </div>
                         <div class="space-y-2">
-                            <h2 class="font-semibold text-[#1F2937]">${plantCards.name}</h2>
+                            <h2 class="font-semibold text-[#1F2937]" onclick="displayModal(${plantCards.id})" >${plantCards.name}</h2>
                             <p class="font-normal text-sm text-[#1F2937] line-clamp-2">${plantCards.description}</p>
                             <div class="flex justify-between">
 
@@ -98,3 +98,47 @@ const allPlantCards = (cards) => {
     });
 }
 allPlant()
+
+// all tress button;
+const allPlantBtn = document.getElementById('all-plant-btn')
+
+allPlantBtn.onclick = () => {
+    showLoading()
+    const allSectionButton = document.querySelectorAll('.all-tress-btn')
+
+    allSectionButton.forEach(btn => {
+
+        btn.classList.remove('bg-[#15803D]', 'text-white')
+    });
+    allPlantBtn.classList.add('bg-[#15803D]', 'text-white')
+
+    hiddenSpinner()
+    allPlant()
+}
+
+
+// modal section;
+const myModal = document.getElementById('my_modal_3')
+const modalTitle = document.getElementById('modal-title')
+const modalImg = document.getElementById('modal-img')
+const categoryDetails = document.getElementById('category-details')
+const modalDescription = document.getElementById('modal-description');
+const modalPrice = document.getElementById('modal-price')
+
+const displayModal = (treeId) => {
+    const url = `https://openapi.programming-hero.com/api/plant/${treeId}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => modal(data.plants))
+
+    myModal.showModal()
+}
+
+const modal = (modalId) => {
+    // console.log(modalId);
+    modalTitle.innerText = modalId.name;
+    modalImg.src = modalId.image;
+    categoryDetails.innerText =  modalId.category;
+    modalDescription.innerText = modalId.description;
+    modalPrice.innerText = modalId.price;
+}
